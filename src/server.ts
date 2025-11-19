2ef6a7b1d15d076d074e717c80d9e4977bbfc1e0
ariful <arifulislam321054@gmail.com>
2025-11-19 23:06:09 +0600

    script added

--- src/server.ts
+++ src/server.ts
@@ -2,17 +2,33 @@ import mongoose from 'mongoose';
 import config from './app/config';
 import app from './app';
 
-async function main() {
-  try {
+let isConnected = false; // prevent multiple connections in serverless
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export default async function handler(req: any, res: any) {
+  if (!isConnected) {
     await mongoose.connect(config.database_url as string);
-    app.listen(config.port, () => {
-      // eslint-disable-next-line no-console
-      console.log(`Example app listening on port ${config.port}`);
-    });
-  } catch (error) {
-    // eslint-disable-next-line no-console
-    console.log(error);
+    isConnected = true;
   }
+
+  return app(req, res);
 }
 
-main();
+// import mongoose from 'mongoose';
+// import config from './app/config';
+// import app from './app';
+
+// async function main() {
+//   try {
+//     await mongoose.connect(config.database_url as string);
+//     app.listen(config.port, () => {
+//       // eslint-disable-next-line no-console
+//       console.log(`Example app listening on port ${config.port}`);
+//     });
+//   } catch (error) {
+//     // eslint-disable-next-line no-console
+//     console.log(error);
+//   }
+// }
+
+// main();
